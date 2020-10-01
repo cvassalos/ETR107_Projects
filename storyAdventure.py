d87f5d70a024a2c9f8a197012c31d84d0f5d9cb6
cvassalos <31630832+cvassalos@users.noreply.github.com>
2020-10-01 15:29:22 -0400

    Add files via upload
    Working version of adventure game

--- storyAdventure.py
+++ storyAdventure.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Thu Oct  1 12:12:33 2020
+
+@author: constantinos
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Sep 23 15:24:48 2020
 
 @author: constantinos
@@ -26,12 +34,18 @@ import time
 print("Slay the Dragon!")
 print("")
 
-playerWeapon = ""
-path = ""
-choice2 = 0
-choice1 = 0
 
-def part1():
+
+
+
+def story():
+    playerWeapon = ""
+    path = ""
+    choice1 = 0
+    choice2 = 0
+    choice3 = 0
+    
+####part one
     print("Your village has been attacked by the local Dragon one too many times, and you have decided enough is enough.")
     time.sleep(4)
     choice1 = input("Choose your weapon of revenge! \n 1 Sword \n 2 Spear \n 3 Bow and Arrow \n 4 A tea kettle \n")
@@ -43,62 +57,66 @@ def part1():
         playerWeapon = "bow and arrow"
     if choice1 == 4:
         playerWeapon = "teakettle"
-    print("You grab your weapon and leave the village with the intent of putting a stop to this menace.")
+    print("You grab your " + playerWeapon + " and leave the village with the intent of putting a stop to this menace.")
     print("")
     time.sleep(6)
 
-def part2():
+####part two
     print("On the village's edge you come to a fork in the road. To the left is a clear road, easier traveling but also much longer.")
     print("To the right is a small path through the wilderness. This is the more direct, but also dangerous path to the awaiting dragon.")
     coinFlip = random.randint(1, 2)
-    input("Type flip to take your chance on a coin flip")
+    input("Type flip to take your chances on a coin \n")
+    time.sleep(2)
     if coinFlip == 1:
         path = "left"
         coinResult = "heads"
-        print("Heads. You begin taking the longer path to the left")
+        print("Heads\n You begin taking the longer path to the left")
         time.sleep(6)
     if coinFlip == 2:
         path = "right"
         coinResult = "tails"
-        print("Tails. You rush into the forest")
         time.sleep(3)
+        print("Tails. \n You rush into the forest")
+        time.sleep(3)
+        print("")
     print("All paths lead eventually lead to the same place, right?")
+    time.sleep(4)
 
-def part3():
-    print("You are well on your journey at this point.")
+####part three
+    print("You are well on your journey at this point. \n ")
     if path == "left":
         print("You come across a stranger on the side of the road. His wagon has a broken axel.")
-        choice2 = int(input("Do you stop to help them? \n 1 Yes \n No"))
+        choice2 = int(input("Do you stop to help them? \n 1 Yes \n 2 No\n"))
         if choice2 == 1:
             print("You stop the help the stranger")
             time.sleep(5)
-            print("The stranger thanks you and joins you on your journey. He, too, has a been the victim of the dragon.")
+            print("The stranger thanks you and joins you on your journey. He, too, has a been the victim of the dragon.\n")
         if choice2 == 2:
             print("There's no time to help this unfortunate fellow, I have a dragon to defeat.")
     if path == "right":
         print("Struggling through the dense forest, you come across a starving wolf. Your rations are low.")
-        choice2 = int(input("Is it a good idea to share your remaining rations with the wolf? \n 1 Yes \n 2 No"))
+        choice2 = int(input("Do you share your remaining rations with the wolf? \n 1 Yes \n 2 No \n"))
         if choice2 == 1:
             print("The wolf greedily eats the jerky you give it. When breaking camp, it follows you, seeminly obedient.")
-            time.time(2)
+            time.sleep(2)
         if choice2 == 2:
             print("You leaving the starving wolf to its doom.")
             time.sleep(1)
-    print("You continue your journey.")
+    print("You continue your journey. \n")
     time.sleep(random.randint(3, 9))
 
-def part4():
+####part four
     print("You finally make it to the dragon's lair and with great confidence, you enter. The beast is there, looking expectant of your arrival.")
-    choice3 = int(input("What do you do? 1 Fight! 2 Talk"))
+    choice3 = int(input("What do you do? 1 Fight! 2 Talk \n"))
     if choice3 == 1:
         print("You charge at the dragon with your " + playerWeapon + " drawn!")
         if choice1 == 4:
-            print("The teakettle breaks into tiny pieces after being smashed into the dragon's  scales. With an annoyed look, the dragon devours you in one bite.")
-        if choice1 == 1 or 2 or 3:
+            print("The teakettle breaks into tiny pieces after being smashed into the dragon's scales. With an annoyed look, the dragon devours you in one bite.")
+        elif choice1 == 1 or 2 or 3:
             if choice2 == 1:
                 print("With the help of your companion you slay the dragon. Now everyone may live in peace!")
             if choice2 == 2:
-                time(5)
+                time.sleep(5)
                 print("It is a long and difficult fight, but all alone the  dragon proves to be too much. If only you had someone, or something to help you in the fight. Your bones are added to the dragon's collection in the lair.")
     if choice3 == 2:
         print("Maybe peace is the best answer. You begin a dialogue with the dragon, who suprisingly speaks the same language as you.")
@@ -112,16 +130,11 @@ def part4():
                 print("The dragon sees you are trustworthy judging by your loyal companion. He trusts you despite your fearsome weapon and agrees to cause no more harm to the village.")
             if choice2 == 2:
                 print("The dragon considers your words of peace.")
-                time(4)
+                time.sleep(4)
                 print("But in the end he devours you as you are armed and alone. One less hero to try to ruin the dragon's fun.")
 
 
-def story():
-    part1()
-    part2()
-    part3()
-    part4()
-    print("I hope you enjoyed your adventure!")
+
 
 story()
 

03ca5ef15b11d19b8aa43da53a0019d05f864618
cvassalos <31630832+cvassalos@users.noreply.github.com>
2020-11-05 12:23:47 -0500

    Add files via upload

--- TempMicroBit/temphumid.py
+++ TempMicroBit/temphumid.py
@@ -7,12 +7,39 @@ sensor = am2320.AM2320(i2c)
 tempReadings = []
 currTemp = 0
 
+
+def tempCompare():
+    """Compares the temperature readings of the MicroBit and AM232"""
+    am2320_temp = 0
+    microBit_temp = 0
+    
+    sensor.measure()
+    am2320 = sensor.temperature()
+    microBit_temp = temperature()
+    
+    if am2320_temp > microBit_temp:
+        display.scroll("AM2320 reads a higher temperature")
+    if am2320_temp < microBit_temp:
+        display.scroll("MicroBit reads a higher temperature")
+    else:
+        display.scroll("The sensors read the same temperature")
+        
+
+def tempRecordings():
+    """Displays recorded temperatures"""
+    for i in tempReadings:
+        display.scroll(i + "   ")
+
+
 while True:
     if button_a.is_pressed():
         sensor.measure()
         currTemp = str(sensor.temperature())
-        display.scroll(str(currTemp+"c"))
+        currHumidity = str(sensor.humidity())
+        display.scroll("Temp = " + str(currTemp+"c     ") + "Humidity = " + currHumidity)
         tempReadings.append(currTemp)
     if button_b.is_pressed():
-        sensor.measure()
-        display.scroll(str(sensor.humidity()) + "%")
+        tempRecordings()
+ 
+        
+ 
